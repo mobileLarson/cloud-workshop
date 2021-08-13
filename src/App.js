b01012281d426ae4308846f09ef53b372d3fcb83
Lars Roewekamp <lars.roewekamp@openknoweldge.de>
2021-08-13 12:46:44 +0200

    changes for v2

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+         React Demo APP V2
         <a
           className="App-link"
           href="https://reactjs.org"
