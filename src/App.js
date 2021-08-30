daa03c727306a90ab112198d8450d598a1184ed5
Lars Roewekamp <lars.roewekamp@openknoweldge.de>
2021-08-30 18:20:38 +0200

    version changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Amplified React App v2 
+          Amplified React App v4
         </p>
         <a
           className="App-link"
